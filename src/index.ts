9ef5830370dd7e07d9c24966d5003e3ae35b1805
亀ハク <kamehaku@kamehakunoMacBook-Pro.local>
2023-10-28 18:52:57 +0900

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { OpenAI } from "langchain/llms/openai";
 import { PromptTemplate } from "langchain/prompts";
 import { BufferMemory } from "langchain/memory";
 import { ConversationChain } from "langchain/chains";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { Document } from "langchain/document";
+
 
 
 require("dotenv").config();
@@ -16,7 +19,10 @@ export const run = async () => {
   const llm_1 = new OpenAI({ temperature: 1, modelName: "gpt-3.5-turbo-16k" });
   const memory = new BufferMemory();
   const chain = new ConversationChain({ llm: llm_1, memory });
-  
+  const loader = new TextLoader("./src/document_loaders/example_data/example.txt");
+  const docs = await loader.load();
+  const doc = new Document({ pageContent: docs[0].pageContent, metadata: { source: "example.txt" } });
+
   const prompt = new PromptTemplate({
     inputVariables: ["question"],
     template: "{question}がわからないようなので教えて",
@@ -28,7 +34,9 @@ export const run = async () => {
         rl.close();
         return;
       }
-      const response = await chain.call({ input: nextInput });
+      // docs（Documentオブジェクト）とnextInputを結合
+      const combinedInput = `${nextInput}\n関連情報: ${doc.pageContent}`;
+      const response = await chain.call({ input: combinedInput });
       console.log(`回答: ${response.response}`);
       continueConversation();
     });
@@ -36,7 +44,8 @@ export const run = async () => {
 
   rl.question('何について質問しますか？ ', async (initialInput) => {
     const initialPrompt = await prompt.format({ question: initialInput });
-    const initialResponse = await chain.call({ input: initialPrompt });
+    const combinedInitialInput = `${initialPrompt}\n関連情報: ${doc.pageContent}`;
+    const initialResponse = await chain.call({ input: combinedInitialInput });
     console.log(`回答: ${initialResponse.response}`);
     continueConversation();
   });
